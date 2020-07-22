e7c91a1b8233cf0acdd139a9b568db5e0c11ee15
Jimmy Lam <dev.jimmylam@gmail.com>
2020-07-22 14:46:33 -0700

    created methods for tic tac toe.

--- App.js
+++ App.js
@@ -2,9 +2,37 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isCross: false,
+      winMessage: ""
+    };
+  }
+
+  drawItem = () => {
+    // TODO: decide what to draw: circle or cross or default
+  };
+
+  chooseItemIcon = () => {
+    // TODO: choose appropriate icon
+  };
+
+  chooseItemColor = () => {
+    // TODO: choose color for item 
+  };
+
+  resetGame = () => {
+    // TODO: reset all values and state
+  };
+
+  winGame = () => {
+    // TODO:  decide/check winner
+  };
+
   render() {
     return (
-      <View style={StyleSheet.container}>
+      <View style={styles.container}>
         <Text>Tic Tac Toe</Text>
       </View>
     );
@@ -15,6 +43,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
 
   }
 })
