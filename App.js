b8f346710dcbdef2b47922806687bdad2568adad
Jimmy Lam <dev.jimmylam@gmail.com>
2020-07-22 20:48:16 -0700

    added UI color palate

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 import { Button } from "native-base";
 
 
-var itemArray = new Array(9).fill('empty');
+var itemArray = new Array(9).fill("empty");
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -15,46 +15,200 @@ export default class App extends React.Component {
   }
 
   drawItem = itemNumber => {
-    //[ x ] TODO: decide what to draw: circle or cross or default
-    if (itemArray[itemNumber] === 'empty') {
+    if (itemArray[itemNumber] === "empty") {
       itemArray[itemNumber] = this.state.isCross;
       this.setState({ isCross: !itemArray[itemNumber] }, () => { });
     }
-    //check for win
 
+    this.winGame();
   };
 
   chooseItemIcon = itemNumber => {
-    //[ x ]  TODO: choose appropriate icon
     if (itemArray[itemNumber] !== "empty") {
-      return itemArray[itemNumber] ? "cross" : "circle"
+      return itemArray[itemNumber] ? "cross" : "circle";
     }
     return "pencil";
   };
 
   chooseItemColor = itemNumber => {
-    // [ x ]  TODO: choose color for item 
     if (itemArray[itemNumber] !== "empty") {
-      return itemArray[itemNumber] ? "red" : "green"
+      return itemArray[itemNumber] ? "red" : "green";
     }
     return "black";
   };
 
   resetGame = () => {
-    //[ x ]  TODO: reset all values and state
     itemArray.fill("empty");
-    this.setState({ winMessage: " " })
-    // TODO: force update to the component
+    this.setState({ winMessage: "" });
+    //force update to the component
+    this.forceUpdate();
   };
 
   winGame = () => {
-    //[ x ] TODO:  decide/check winner
+    if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] == itemArray[1] &&
+      itemArray[1] == itemArray[2]
+    ) {
+      this.setState({
+        winMessage: (itemArray[0] ? "Cross" : "Circle").concat(" win")
+      });
+    } else if (
+      itemArray[3] !== "empty" &&
+      itemArray[3] == itemArray[4] &&
+      itemArray[4] == itemArray[5]
+    ) {
+      this.setState({
+        winMessage: (itemArray[0] ? "Cross" : "Circle").concat(" win")
+      });
+    } else if (
+      itemArray[6] !== "empty" &&
+      itemArray[6] == itemArray[7] &&
+      itemArray[7] == itemArray[8]
+    ) {
+      this.setState({
+        winMessage: (itemArray[6] ? "Cross" : "Circle").concat(" win")
+      });
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] == itemArray[3] &&
+      itemArray[3] == itemArray[6]
+    ) {
+      this.setState({
+        winMessage: (itemArray[0] ? "Cross" : "Circle").concat(" win")
+      });
+    } else if (
+      itemArray[1] !== "empty" &&
+      itemArray[1] == itemArray[4] &&
+      itemArray[4] == itemArray[7]
+    ) {
+      this.setState({
+        winMessage: (itemArray[1] ? "Cross" : "Circle").concat(" win")
+      });
+    } else if (
+      itemArray[2] !== "empty" &&
+      itemArray[2] == itemArray[5] &&
+      itemArray[5] == itemArray[8]
+    ) {
+      this.setState({
+        winMessage: (itemArray[2] ? "Cross" : "Circle").concat(" win")
+      });
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] == itemArray[4] &&
+      itemArray[4] == itemArray[8]
+    ) {
+      this.setState({
+        winMessage: (itemArray[0] ? "Cross" : "Circle").concat(" win")
+      });
+    } else if (
+      itemArray[2] !== "empty" &&
+      itemArray[2] == itemArray[4] &&
+      itemArray[4] == itemArray[6]
+    ) {
+      this.setState({
+        winMessage: (itemArray[2] ? "Cross" : "Circle").concat(" win")
+      });
+    }
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Tic Tac Toe</Text>
+        <Text style={styles.txttitle}>Tic Tac Toe</Text>
+        <View style={styles.grid}>
+          <View style={styles.row}>
+            <View style={styles.item}>
+              <TouchableOpacity onPress={() => this.drawItem(0)}>
+                <Entypo
+                  name={this.chooseItemIcon(0)}
+                  size={50}
+                  color={this.chooseItemColor(0)}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.item}>
+              <TouchableOpacity onPress={() => this.drawItem(1)}>
+                <Entypo
+                  name={this.chooseItemIcon(1)}
+                  size={50}
+                  color={this.chooseItemColor(1)}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.item}>
+              <TouchableOpacity onPress={() => this.drawItem(2)}>
+                <Entypo
+                  name={this.chooseItemIcon(2)}
+                  size={50}
+                  color={this.chooseItemColor(2)}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.item}>
+              <TouchableOpacity onPress={() => this.drawItem(3)}>
+                <Entypo
+                  name={this.chooseItemIcon(3)}
+                  size={50}
+                  color={this.chooseItemColor(3)}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.item}>
+              <TouchableOpacity onPress={() => this.drawItem(4)}>
+                <Entypo
+                  name={this.chooseItemIcon(4)}
+                  size={50}
+                  color={this.chooseItemColor(4)}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.item}>
+              <TouchableOpacity onPress={() => this.drawItem(5)}>
+                <Entypo
+                  name={this.chooseItemIcon(5)}
+                  size={50}
+                  color={this.chooseItemColor(5)}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.item}>
+              <TouchableOpacity onPress={() => this.drawItem(6)}>
+                <Entypo
+                  name={this.chooseItemIcon(6)}
+                  size={50}
+                  color={this.chooseItemColor(6)}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.item}>
+              <TouchableOpacity onPress={() => this.drawItem(7)}>
+                <Entypo
+                  name={this.chooseItemIcon(7)}
+                  size={50}
+                  color={this.chooseItemColor(7)}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.item}>
+              <TouchableOpacity onPress={() => this.drawItem(8)}>
+                <Entypo
+                  name={this.chooseItemIcon(8)}
+                  size={50}
+                  color={this.chooseItemColor(8)}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <Text style={styles.winMessage}>{this.state.winMessage}</Text>
+        <Button full rounded style={styles.button}>
+          <Text style={styles.btntext}>Reset Game</Text>
+        </Button>
       </View>
     );
   }
@@ -63,9 +217,36 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#6A89CC",
     alignItems: "center",
     justifyContent: "center"
-
+  },
+  txttitle: {
+    color: "#4BCFFA",
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  grid: {},
+  row: {
+    flexDirection: "row"
+  },
+  item: {
+    borderWidth: 2,
+    borderColor: "#000",
+    padding: 30
+  },
+  winMessage: {
+    padding: 20,
+    fontSize: 25,
+    fontWeight: "bold"
+  },
+  button: {
+    margin: 20,
+    padding: 10,
+    backgroundColor: "#4BCFFA",
+  },
+  btntext: {
+    color: "#FFF",
+    fontWeight: "bold"
   }
-})
+});
